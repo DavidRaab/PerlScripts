45624131a05ef1f9b54913ca79bfe9af9ab1a196
David Raab <davidraab83@gmail.com>
2022-11-06 22:23:23 +0100

    Used some newer Perl features
    * signatures
    * postderef

--- Color.pm
+++ Color.pm
@@ -1,14 +1,15 @@
 package Color;
-use strict;
+use v5.32;
 use warnings;
+use feature 'signatures';
+no warnings 'experimental::signatures';
 use Term::ANSIColor qw(colored);
 use Sub::Exporter -setup => {
     exports => [ qw(black red green yellow blue magenta cyan white) ],
 };
 
 # creates a new function with color partial applied
-sub create_colored {
-    my ($color) = @_;
+sub create_colored ($color) {
     return sub {
         my ($str) = @_;
         return colored([$color], $str);
--- SyncConfig.pm
+++ SyncConfig.pm
@@ -1,14 +1,14 @@
 package SyncConfig;
-use strict;
+use v5.32;
 use warnings;
-use DDP;
+#use DDP;
 use Path::Class;
 
 # Helper Functions to create data-structure
 sub sync {
     my ( $destination ) = @_;
     $destination = dir($destination);
-    
+
     my $music = dir("/mnt/daten/Musik");
     return sub {
         my ( $folder, $method ) = @_;
@@ -24,17 +24,21 @@ sub sync {
 *sd = sync("/media/david/A770-7A18");
 
 my @synchronize = (
-    d2("2019"), d2("2020"), d2("2021"), d2("2022-1"), d2("2022-2"), d2("2wei"), d2("8 Graves"), 
+    d2("2019"), d2("2020"), d2("2021"), d2("2022-1"), d2("2022-2"), d2("2wei"), d2("8 Graves"),
     d2("Adele"), d2("Apashe"),
     d2("Best of Yiruma"),
     d2("Daft Punk"), d2("Dub Fx"),
     d2("Enigma"), d2("E Nomine"), d2("Enya/Very Best of Enya"),
     d2("Foo Fighters"), d2("Gorillaz"), d2("Hidden Citizens"), d2("Infected Mushroom"), d2("INZO"),
-    sd("Juno Reactor"), d2("Justice"), d2("Linkin Park"),
+    sd("Juno Reactor"), d2("Justice"),
+    d2("Linkin Park"),
     d2("Massive Attack"), d2("Milow"), d2("Moby"), d2("Nintendo"),
     d2("Games" => ["Wasteland 3 Radio Songs.mp3", "Fallout 4 - All Magnolia Songs.mp3", "Transistor Original Soundtrack.mp3", "Doom Eternal.mp3"]),
 #    d2("Games/Bastion Original Soundtrack"),  TODO: Correct Handling
     sd("Keep Calm And Feel The Reggae"),
+    sd("MS MR"),
+    d2("Secession Studios"),
+    sd("The Heavy Horses"),
 );
 
 sub config {
