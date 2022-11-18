a760f9dc10a306f4038dfd303a345ffc85635907
David Raab <davidraab83@gmail.com>
2022-11-18 19:40:17 +0100

    Updated Config

--- SyncConfig.pm
+++ SyncConfig.pm
@@ -27,7 +27,7 @@ my @synchronize = (
     d2("2019"), d2("2020"), d2("2021"), d2("2022-1"), d2("2022-2"), d2("2wei"), d2("8 Graves"),
     d2("Adele"), d2("Apashe"),
     d2("Best of Yiruma"),
-    d2("Daft Punk"), d2("Dub Fx"),
+    d2("Daft Punk"), d2("Dub Fx"), sd("Dark Country 4"),
     d2("Enigma"), d2("E Nomine"), d2("Enya/Very Best of Enya"),
     d2("Foo Fighters"), d2("Gorillaz"), d2("Hidden Citizens"), d2("Infected Mushroom"), d2("INZO"),
     sd("Juno Reactor"), d2("Justice"),
